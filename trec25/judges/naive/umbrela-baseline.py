219a0b4fd819b6978167f6be45042b97018ef6df
Laura <dietz@cs.unh.edu>
2025-12-12 23:42:49 -0500

    Umbrela-baseline: refactor
    bugfixes (used old run_id, topic_id variables)
    and subsequent refactoring of code to avoid regression

--- trec25/judges/naive/umbrela-baseline.py
+++ trec25/judges/naive/umbrela-baseline.py
@@ -21,6 +21,7 @@ from pydantic import BaseModel
 
 
 class UmbrelaAnnotation(BaseModel):
+    run_id:str
     query_id:str
     title_query:str
     source_document:str
@@ -41,47 +42,62 @@ def main(rag_responses: list[dict], rag_topics: List[Request], output: Path):
     """
     A naive rag response assessor that just orders each response by its length.
     """
-    ret = []
-    avg_grades = defaultdict(list)
-    avg_ismatch = defaultdict(list)
     topic_dict = {request.request_id: request for request in rag_topics}
-    alignment_input_list = list()
 
-    for rag_response in rag_responses:
-        metadata = rag_response["metadata"]
-        run_id = metadata["run_id"]
-        topic_id = metadata["narrative_id"]
+    def prepare_prompts()->List[UmbrelaAnnotation]:
+        alignment_input_list = list()
+        for rag_response in rag_responses:
+            metadata = rag_response["metadata"]
+            run_id = metadata["run_id"]
+            topic_id = metadata["narrative_id"]
+            
+            text = " ".join([i["text"] for i in rag_response["answer"]])
+            
+            topic = topic_dict[topic_id]
+            if topic is None:
+                raise RuntimeError("Could not identify request object for topic {topic_id}")
+            
+            if (topic.title is None) or (topic.background is None) or  (topic.problem_statement is None):
+                raise RuntimeError(f"Missing fields in report request: title {topic.title}, background:{topic.background}, problem_statement: {topic.problem_statement}.")
+            
+            prompt_objs = UmbrelaAnnotation(query_id = topic_id
+                                                , run_id = run_id
+                                                ,source_document = text
+                                                ,metadata= metadata
+                                                ,title_query = topic.title
+                                                ,background = topic.background
+                                                ,problem_statement =  topic.problem_statement
+                                                )
+            alignment_input_list.append(prompt_objs)
+        return alignment_input_list
+
+
+    def write_results(prompt_output:List[UmbrelaAnnotation]):
+        ret = []
+        avg_grades = defaultdict(list)
+        avg_ismatch = defaultdict(list)
         
-        text = " ".join([i["text"] for i in rag_response["answer"]])
-        
-        topic = topic_dict[topic_id]
-        if topic is None:
-            raise RuntimeError("Could not identify request object for topic {topic_id}")
-        
-        alignment_inputs = UmbrelaAnnotation(query_id = topic_id
-                                            ,source_document = text
-                                            ,metadata= metadata
-                                            ,title_query = topic.title if topic.title else ""
-                                            ,background = topic.background if topic.background else ""
-                                            ,problem_statement =  topic.problem_statement if topic.problem_statement else ""
-                                            )
-        alignment_input_list.append(alignment_inputs)
+        for res in prompt_output:
+            topic_id = res.query_id
+            run_id = res.run_id
+            ret.append(f"{run_id} GRADE {topic_id} {res.match_score}")
+            ret.append(f"{run_id} IS_MATCH {topic_id} {res.is_match}")
+            avg_grades[run_id].append(res.match_score)
+            avg_ismatch[run_id].append(res.is_match)
+            
+        ret.append(f"{run_id} GRADES all {mean([float(g) for g in avg_grades[run_id]])}")
+        ret.append(f"{run_id} ISMATCH all {mean([1.0 if b else 0.0 for b in avg_ismatch[run_id]])}")
 
+        output.parent.mkdir(exist_ok=True, parents=True)
+        output.write_text("\n".join(ret))
 
-    alignment_results =  evaluator_run(prompt=Umbrela, output_converter=Umbrela.convert_output,alignment_input_list=alignment_input_list)
-       
-    for res in alignment_results:
-        ret.append(f"{run_id} GRADE {topic_id} {res.match_score}")
-        ret.append(f"{run_id} IS_MATCH {topic_id} {res.is_match}")
-        avg_grades[run_id].append(res.match_score)
-        avg_ismatch[run_id].append(res.is_match)
-        
-    ret.append(f"{run_id} GRADES all {mean([float(g) for g in avg_grades[run_id]])}")
-    ret.append(f"{run_id} ISMATCH all {mean([1.0 if b else 0.0 for b in avg_ismatch[run_id]])}")
+    prompt_input = prepare_prompts()
+    print("in", "\n".join(str(p) for p in prompt_input[0:4]))
+    
+    prompt_output =  evaluator_run(prompt=Umbrela, output_converter=Umbrela.convert_output,alignment_input_list=prompt_input)
+    print("out", "\n".join(str(p) for p in prompt_input[0:4]))
 
-    output.parent.mkdir(exist_ok=True, parents=True)
-    output.write_text("\n".join(ret))
-
+    write_results(prompt_output=prompt_output)
 
 if __name__ == '__main__':
     main()
