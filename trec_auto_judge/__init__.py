62b5dd5ff43592906ab7f4e892e8748bd16f16b7
Maik Fröbe <maik.froebe@uni-jena.de>
2025-12-17 08:21:04 +0100

    intermediate commit to get ci working again

--- trec_auto_judge/__init__.py
+++ trec_auto_judge/__init__.py
@@ -1,10 +1,5 @@
 from click import group
-from typing import Protocol, Sequence, Optional
-
-from .report import Report, load_report
-from .request import Request, load_requests_from_irds, load_requests
-from .leaderboard import Leaderboard, LeaderboardEntry, MeasureDef, MeasureName, write_leaderboard
-from .qrels import Qrels, QrelEntry, write_qrel_file, read_qrel_file
+from .request import Request, load_requests_from_irds, load_requests_from_file
 from ._commands._evaluate import evaluate
 
 __version__ = '0.0.1'
@@ -17,14 +12,6 @@ def main():
 
 main.command()(evaluate)
 
-
-class AutoJudge(Protocol):
-    def judge(
-        self,
-        rag_responses: Sequence["Report"],
-        rag_topics: Sequence["Request"],
-    ) -> tuple["Leaderboard", Optional["Qrels"]]:
-        ...
 
 if __name__ == '__main__':
     main()
