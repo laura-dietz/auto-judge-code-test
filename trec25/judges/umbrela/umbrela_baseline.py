47a29fd980f9a05568bfc7c502e6f6ca46acac6c
Laura <dietz@cs.unh.edu>
2025-12-17 00:42:45 -0500

    wrapping the UmbrelaJudge in the AutoJudge Protocol
    This is going to be the future interface for auto-judges
    What is currently in def main() should move to a more global place
    main() will become an optional dev-tool

--- trec25/judges/umbrela/umbrela_baseline.py
+++ trec25/judges/umbrela/umbrela_baseline.py
@@ -12,6 +12,7 @@ from trec_auto_judge.request import Request
 from trec_auto_judge.report import Report
 from trec_auto_judge.leaderboard import *
 from trec_auto_judge.qrels import *
+from trec_auto_judge import AutoJudge
 
 # crucible
 from nuggety.align import evaluator_run, Umbrela
@@ -59,68 +60,73 @@ def umbrela_to_qrels(
         )
     return qrels
 
+class UmbrelaJudge:
+    def judge(
+        self,
+        rag_responses: Sequence[Report],
+        rag_topics: Sequence[Request],
+    ) -> tuple[Leaderboard, Optional[Qrels]]:
+            
+        """
+        Umbrela response assessor that just orders each response by its length.
+        """
+        topic_dict = {request.request_id: request for request in rag_topics}
 
-def judge(rag_responses: List[Report], rag_topics: List[Request])->Tuple[Leaderboard, Optional[Qrels]]:
-    """
-    Umbrela response assessor that just orders each response by its length.
-    """
-    topic_dict = {request.request_id: request for request in rag_topics}
+        def prepare_prompts()->List[UmbrelaAnnotation]:
+            alignment_input_list = list()
+            for rag_response in rag_responses:
+                metadata = rag_response["metadata"]
+                run_id = metadata["run_id"]
+                topic_id = metadata["narrative_id"]
 
-    def prepare_prompts()->List[UmbrelaAnnotation]:
-        alignment_input_list = list()
-        for rag_response in rag_responses:
-            metadata = rag_response["metadata"]
-            run_id = metadata["run_id"]
-            topic_id = metadata["narrative_id"]
+                text = " ".join([i["text"] for i in rag_response["answer"]])  # todo use the Report format
 
-            text = " ".join([i["text"] for i in rag_response["answer"]])  # todo use the Report format
+                topic = topic_dict[topic_id]
+                if topic is None:
+                    raise RuntimeError("Could not identify request object for topic {topic_id}")
 
-            topic = topic_dict[topic_id]
-            if topic is None:
-                raise RuntimeError("Could not identify request object for topic {topic_id}")
+                if topic.title is None:
+                    raise RuntimeError(f"Missing fields in report request: title {topic.title}, background:{topic.background}, problem_statement: {topic.problem_statement}.")
 
-            if topic.title is None:
-                raise RuntimeError(f"Missing fields in report request: title {topic.title}, background:{topic.background}, problem_statement: {topic.problem_statement}.")
+                problem_statement = topic.problem_statement if topic.problem_statement else f"Identify information that is relevant to the query {topic.title}"
+                background = topic.background if topic.background else f"I want to find relevant information on {topic.title}"
 
-            problem_statement = topic.problem_statement if topic.problem_statement else f"Identify information that is relevant to the query {topic.title}"
-            background = topic.background if topic.background else f"I want to find relevant information on {topic.title}"
-
-            prompt_objs = UmbrelaAnnotation(query_id = topic_id
-                                            , run_id = run_id
-                                            ,source_document = text
-                                            ,metadata= metadata
-                                            ,title_query = topic.title
-                                            ,background = background
-                                            ,problem_statement = problem_statement
-                                            )
-            alignment_input_list.append(prompt_objs)
-        # return (alignment_input_list, None)
-        return alignment_input_list
+                prompt_objs = UmbrelaAnnotation(query_id = topic_id
+                                                , run_id = run_id
+                                                ,source_document = text
+                                                ,metadata= metadata
+                                                ,title_query = topic.title
+                                                ,background = background
+                                                ,problem_statement = problem_statement
+                                                )
+                alignment_input_list.append(prompt_objs)
+            # return (alignment_input_list, None)
+            return alignment_input_list
 
 
-    def umbrela_to_leaderboard(prompt_output: Iterable["UmbrelaAnnotation"], measures) -> Leaderboard:
-            per_topic_entries = [
-                LeaderboardEntry(
-                    run_id=res.run_id,
-                    topic_id=res.query_id,
-                    values={
-                        "GRADE": float(res.match_score),
-                        "IS_MATCH": bool(res.is_match),
-                    },
-                )
-                for res in prompt_output
-            ]
-            return Leaderboard.from_entries_with_all(measures=measures, entries=per_topic_entries)
+        def umbrela_to_leaderboard(prompt_output: Iterable["UmbrelaAnnotation"], measures) -> Leaderboard:
+                per_topic_entries = [
+                    LeaderboardEntry(
+                        run_id=res.run_id,
+                        topic_id=res.query_id,
+                        values={
+                            "GRADE": float(res.match_score),
+                            "IS_MATCH": bool(res.is_match),
+                        },
+                    )
+                    for res in prompt_output
+                ]
+                return Leaderboard.from_entries_with_all(measures=measures, entries=per_topic_entries)
 
-    prompt_input = prepare_prompts()
-    print("Debug in", "\n".join(str(p) for p in prompt_input[0:1]))
-    
-    prompt_output = evaluator_run(prompt=Umbrela, output_converter=Umbrela.convert_output, alignment_input_list=prompt_input)
-    print("Debug out", "\n".join(str(p) for p in prompt_input[0:1]))
+        prompt_input = prepare_prompts()
+        print("Debug in", "\n".join(str(p) for p in prompt_input[0:1]))
+        
+        prompt_output = evaluator_run(prompt=Umbrela, output_converter=Umbrela.convert_output, alignment_input_list=prompt_input)
+        print("Debug out", "\n".join(str(p) for p in prompt_input[0:1]))
 
-    leaderboard = umbrela_to_leaderboard(prompt_output=prompt_output, measures = MEASURES)
-    qrels = umbrela_to_qrels(  prompt_output, grade_fn=lambda res: res.answerability)
-    return (leaderboard, qrels)
+        leaderboard = umbrela_to_leaderboard(prompt_output=prompt_output, measures = MEASURES)
+        qrels = umbrela_to_qrels(  prompt_output, grade_fn=lambda res: res.answerability)
+        return (leaderboard, qrels)
 
 # below here all should move into the TIRA CLI, or be a main class with its own CLI for development
 
@@ -130,7 +136,7 @@ def judge(rag_responses: List[Report], rag_topics: List[Request])->Tuple[Leaderb
 @click.option("--output", type=Path, help="The output file.", required=True)
 def main(rag_responses: List[Report], rag_topics: List[Request], output:Path):
     qrels_opt=None
-    (leaderboard, qrels_opt) = judge(rag_responses=rag_responses, rag_topics=rag_topics)
+    (leaderboard, qrels_opt) = UmbrelaJudge().judge(rag_responses=rag_responses, rag_topics=rag_topics)
     write_leaderboard(leaderboard=leaderboard, output=output)
     if qrels_opt is not None:
         write_qrel_file(qrel_out_file=output.with_suffix(".qrels"), qrel_entries= qrels_opt)
--- trec_auto_judge/__init__.py
+++ trec_auto_judge/__init__.py
@@ -1,4 +1,13 @@
 from click import group
+from typing import Protocol, Sequence, Optional
+
+class AutoJudge(Protocol):
+    def judge(
+        self,
+        rag_responses: Sequence["Report"],
+        rag_topics: Sequence["Request"],
+    ) -> tuple["Leaderboard", Optional["Qrels"]]:
+        ...
 from .report import Report, load_report
 from .request import Request, load_requests_from_irds, load_requests
 from .leaderboard import Leaderboard, LeaderboardEntry, MeasureDef, MeasureName, write_leaderboard
@@ -15,6 +24,14 @@ def main():
 
 main.command()(evaluate)
 
+
+class AutoJudge(Protocol):
+    def judge(
+        self,
+        rag_responses: Sequence["Report"],
+        rag_topics: Sequence["Request"],
+    ) -> tuple["Leaderboard", Optional["Qrels"]]:
+        ...
 
 if __name__ == '__main__':
     main()
