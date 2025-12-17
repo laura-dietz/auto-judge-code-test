c17b4ed2e384ead1dfb53b68bf9174a25aa340ac
Laura <dietz@cs.unh.edu>
2025-12-17 00:33:47 -0500

    Umbrela with Leaderboard and Qrels writing infra
    * trec_auto_judge/leaderboard.py:  Infrastructure for building and
      writing leaderboards
    * trec_auto_judge/qres.py:  Infrastructure for building and writing
      qrels files
    * trec_auto_judge/request.py: Infrastructure for loading complex RAG topics
    
    * umbrela_baseline.py:  example code for drafting the new `judge`
      function

--- trec_auto_judge/__init__.py
+++ trec_auto_judge/__init__.py
@@ -1,9 +1,12 @@
 from click import group
 from .report import Report, load_report
 from .request import Request, load_requests_from_irds, load_requests
+from .leaderboard import Leaderboard, LeaderboardEntry, MeasureDef, MeasureName, write_leaderboard
+from .qrels import Qrels, QrelEntry, write_qrel_file, read_qrel_file
 from ._commands._evaluate import evaluate
 
 __version__ = '0.0.1'
+
 
 @group()
 def main():
--- trec_auto_judge/click.py
+++ trec_auto_judge/click.py
@@ -1,24 +1,22 @@
 from pathlib import Path
 from .io import load_runs_failsave
-from .request import Request, load_requests_from_irds
-import click
-
-
-class ClickRagResponses(click.ParamType):
-    name = "dir"
-
-    def convert(self, value, param, ctx):
-        if not value or not Path(value).is_dir():
-            self.fail(f"The directory {value} does not exist, so I can not load rag responses from this directory.", param, ctx)
-        runs = load_runs_failsave(Path(value))
-
-        if len(runs) > 0:
-            return runs
-
-        self.fail(f"{value!r} contains no rag runs.", param, ctx)
-
+from .request import Request, load_requests
 
 def option_rag_responses():
+    import click
+    class ClickRagResponses(click.ParamType):
+        name = "dir"
+
+        def convert(self, value, param, ctx):
+            if not value or not Path(value).is_dir():
+                self.fail(f"The directory {value} does not exist, so I can not load rag responses from this directory.", param, ctx)
+            runs = load_runs_failsave(Path(value))
+
+            if len(runs) > 0:
+                return runs
+
+            self.fail(f"{value!r} contains no rag runs.", param, ctx)
+
     """Rag Run directory click option."""
     def decorator(func):
         func = click.option(
@@ -32,93 +30,28 @@ def option_rag_responses():
 
     return decorator
 
-
-class ClickIrDataset(click.ParamType):
-    name = "ir-dataset"
-
-    def convert(self, value, param, ctx):
-        from ir_datasets import registry
-        from tira.third_party_integrations import ir_datasets
-        from .io import irds_from_dir, load_hf_dataset_config_or_none
-
-        if value == "infer-dataset-from-context":
-            candidate_files = set()
-            if "rag_responses" in ctx.params:
-                for r in ctx.params["rag_responses"]:
-                    if "path" in r:
-                        p = Path(r["path"]).parent
-                        candidate_files.add(p / "README.md")
-                        candidate_files.add(p.parent / "README.md")
-            irds_config = None
-            base_path = None
-            for c in candidate_files:
-                irds_config = load_hf_dataset_config_or_none(c, ["ir_dataset"])
-                if irds_config:
-                    base_path = c.parent
-                    irds_config = irds_config["ir_dataset"]
-                    break
-
-            if not irds_config:
-                raise ValueError("ToDo: Better error handling of wrong configurations")
-
-            if "ir_datasets_id" in irds_config:
-                return ir_datasets.load(irds_config["ir_datasets_id"])
-            elif "directory" in irds_config:
-                return irds_from_dir(str(base_path / irds_config["directory"]))
-            else:
-                raise ValueError("ToDo: Better error handling of incomplete configurations")
-
-        if value and value in registry:
-            return ir_datasets.load(value)
-
-        if value and Path(value).is_dir() and (Path(value) / "queries.jsonl").is_file() and (Path(value) / "corpus.jsonl.gz").is_file():
-            return irds_from_dir(value)
-
-        return ir_datasets.load(value)
-
-
-def option_ir_dataset():
-    """Ir-dataset click option."""
-    def decorator(func):
-        func = click.option(
-            "--ir-dataset",
-            type=ClickIrDataset(),
-            required=False,
-            default="infer-dataset-from-context",
-            help="The ir-datasets ID or a directory that contains the ir-dataset or TODO...."
-        )(func)
-
-        return func
-
-    return decorator
-
-
-class ClickRagTopics(ClickIrDataset):
-    name = "ir-dataset"
-
-    def convert(self, value, param, ctx):
-        ds = super().convert(value, param, ctx)
-
-        if not ds:
-            self.fail(f"I could not load the rag topics for ir_dataset {value}.", param, ctx)
-        
-        ret = load_requests_from_irds(ds)
-
-        if len(ret) > 0:
-            return ret
-
-        self.fail(f"{value!r} contains no RAG topics.", param, ctx)
-
-
 def option_rag_topics():
+    import click
+    class ClickRagTopics(click.ParamType):
+        name = "file"
+
+        def convert(self, value, param, ctx):
+            if not value or not Path(value).is_file():
+                self.fail(f"Trying to load RAG topics from {value}, but file does not exist.", param, ctx)
+            topics = load_requests(Path(value))
+
+            if len(topics) > 0:
+                return topics
+
+            self.fail(f"{value!r} contains no RAG topics.", param, ctx)
+
     """Provide RAG topics CLI option."""
     def decorator(func):
         func = click.option(
             "--rag-topics",
             type=ClickRagTopics(),
-            required=False,
-            default="infer-dataset-from-context",
-            help="The ir-datasets ID or a directory that contains the ir-dataset or TODO...."
+            required=True,
+            help="The file that contains run_as_taskd_group topics for evaluation."
         )(func)
 
         return func
