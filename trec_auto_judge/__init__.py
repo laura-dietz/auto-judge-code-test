701b3834b9c0fe03e93ea1413b313cd4cb8fb284
Maik Fröbe <maik.froebe@uni-jena.de>
2025-12-17 08:15:19 +0100

    prepare merge

--- trec_auto_judge/__init__.py
+++ trec_auto_judge/__init__.py
@@ -2,13 +2,6 @@ from click import group
 from .request import Request, load_requests_from_irds, load_requests_from_file, load_report
 from typing import Protocol, Sequence, Optional
 
-class AutoJudge(Protocol):
-    def judge(
-        self,
-        rag_responses: Sequence["Report"],
-        rag_topics: Sequence["Request"],
-    ) -> tuple["Leaderboard", Optional["Qrels"]]:
-        ...
 from .report import Report, load_report
 from .request import Request, load_requests_from_irds, load_requests
 from .leaderboard import Leaderboard, LeaderboardEntry, MeasureDef, MeasureName, write_leaderboard
