f4c65742abeac1370b68597ed1447fb29ec24e38
Laura <dietz@cs.unh.edu>
2025-12-16 20:32:25 -0500

    intermittent changes trying to get new TIRA working

--- trec_auto_judge/__init__.py
+++ trec_auto_judge/__init__.py
@@ -1,6 +1,6 @@
 from click import group
 from .report import Report, load_report
-from .request import Request, load_requests, load_requests_from_irds
+from .request import Request, load_requests_from_irds, load_requests
 from ._commands._evaluate import evaluate
 
 __version__ = '0.0.1'
