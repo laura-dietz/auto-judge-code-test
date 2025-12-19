e1f940f5ac807c09b36a8096f726f63ead8d0617
Maik Fröbe <maik.froebe@uni-jena.de>
2025-12-19 13:50:07 +0100

    adjust pyterrier-retrieval

--- trec25/judges/pyterrier-retrieval/retrieval-judge.py
+++ trec25/judges/pyterrier-retrieval/retrieval-judge.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
-from trec_auto_judge.click import option_rag_responses, option_rag_topics
+from trec_auto_judge import option_rag_responses, Report, LeaderboardSpec, LeaderboardBuilder, mean_of_floats, MeasureSpec, option_rag_topics
 import click
 from pathlib import Path
 from collections import defaultdict
 from tqdm import tqdm
 from statistics import median
 import pandas as pd
-
+from tira.third_party_integrations import ensure_pyterrier_is_loaded
 import pyterrier as pt
 
-def group_by_topic_id(rag_responses: list[dict]) -> float:
+
+def group_by_topic_id(rag_responses: list[Report]) -> float:
     ret = defaultdict(dict)
     for rag_response in rag_responses:
-        metadata = rag_response["metadata"]
-        run_id = metadata["run_id"]
-        topic_id = metadata["narrative_id"]
-        ret[topic_id][run_id] = " ".join([i["text"] for i in rag_response["answer"]])
+        run_id = rag_response.metadata.run_id
+        topic_id = rag_response.metadata.topic_id
+        ret[topic_id][run_id] = rag_response.get_report_text()
     return ret
 
+
 # Some semi-random selected weighting models from http://terrier.org/docs/v4.2/javadoc/org/terrier/matching/models/WeightingModel.html
-WEIGHTING_MODELS = ["BM25", "DirichletLM", "Hiemstra_LM", "DFIC", "DPH", "DLH", "Tf", "TF_IDF", "PL2", "InL2"]
+LEADERBOARD_SPEC = LeaderboardSpec(measures=(
+    MeasureSpec("BM25", aggregate=mean_of_floats, cast=float),
+    MeasureSpec("DirichletLM", aggregate=mean_of_floats, cast=float),
+    MeasureSpec("Hiemstra_LM", aggregate=mean_of_floats, cast=float),
+    MeasureSpec("DFIC", aggregate=mean_of_floats, cast=float),
+    MeasureSpec("DPH", aggregate=mean_of_floats, cast=float),
+    MeasureSpec("DLH", aggregate=mean_of_floats, cast=float),
+    MeasureSpec("Tf", aggregate=mean_of_floats, cast=float),
+    MeasureSpec("TF_IDF", aggregate=mean_of_floats, cast=float),
+    MeasureSpec("PL2", aggregate=mean_of_floats, cast=float),
+    MeasureSpec("InL2", aggregate=mean_of_floats, cast=float),
+))
 
 
 @click.command("retrieval-judge")
@@ -30,40 +42,37 @@ def main(rag_responses: list[dict], rag_topics: list, output: Path):
     """
     fooo.
     """
-    pt.java.init()
+    ensure_pyterrier_is_loaded()
     tokeniser = pt.java.autoclass("org.terrier.indexing.tokenisation.Tokeniser").getTokeniser()
     def pt_tokenize(text):
         return ' '.join(tokeniser.getTokens(text))
 
     topic_id_to_title = {i.request_id: pt_tokenize(i.title) for i in rag_topics}
     topic_id_to_responses = group_by_topic_id(rag_responses)
-    all_systems = set([i["metadata"]["run_id"] for i in rag_responses])
+    all_systems = set([i.metadata.run_id for i in rag_responses])
 
-    ret = []
-    
-    system_to_wmodel_to_scores = defaultdict(lambda: defaultdict(list))
+    ret = LeaderboardBuilder(LEADERBOARD_SPEC)
+
     for topic in tqdm(topic_id_to_responses.keys(), "Process Topics"):
         query_text = topic_id_to_title[topic]
         docs = [{"docno": system, "text": system_response} for system, system_response in topic_id_to_responses[topic].items()]
+        system_to_wmodel_to_score = defaultdict(lambda: defaultdict(lambda: 1000))
         index = pt.IterDictIndexer("/not-needed/for-memory-index", meta={'docno' : 100}, type=pt.IndexingType.MEMORY).index(docs)
 
-        for wmodel in WEIGHTING_MODELS:
-            retriever = pt.terrier.Retriever(index, wmodel=wmodel)
+        for wmodel in LEADERBOARD_SPEC.measures:
+            retriever = pt.terrier.Retriever(index, wmodel=wmodel.name)
             rtr = retriever.search(query_text)
             run_id_to_score = defaultdict(lambda: 0)
             for _, i in rtr.iterrows():
                 run_id_to_score[i["docno"]] = max(0, 1000 - i["rank"])
             
             for system in all_systems:
-                system_to_wmodel_to_scores[system][wmodel].append(run_id_to_score.get(system))
-                ret.append(f"{system} {wmodel} {topic} {run_id_to_score.get(system)}")
+                system_to_wmodel_to_score[system][wmodel.name] = run_id_to_score.get(system)
+        
+        for system in all_systems:
+            ret.add(run_id=system, topic_id=topic, values=system_to_wmodel_to_score[system])
 
-    for run_id in all_systems:
-        for wmodel in WEIGHTING_MODELS:
-            ret.append(f"{run_id} {wmodel} all {median(system_to_wmodel_to_scores[run_id][wmodel])}")
-
-    output.parent.mkdir(exist_ok=True, parents=True)
-    output.write_text("\n".join(ret))
+    ret.build().write(output=output)
 
 
 if __name__ == '__main__':
