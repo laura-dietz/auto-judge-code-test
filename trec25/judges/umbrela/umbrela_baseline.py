3d1d4b6748ed31167d53d6f3ef2ad1df279f8578
Maik Fröbe <maik.froebe@uni-jena.de>
2025-12-22 23:00:56 +0100

    make md5 unique for umbrela

--- trec25/judges/umbrela/umbrela_baseline.py
+++ trec25/judges/umbrela/umbrela_baseline.py
@@ -30,9 +30,9 @@ UMBRELA_SPEC = LeaderboardSpec(measures=(
 
 UMBRELA_QRELS = QrelsSpec["UmbrelaAnnotation"](
     topic_id=lambda r: r.query_id,
-    doc_id=lambda r: doc_id_md5(r.source_document + r.query_id),
+    doc_id=lambda r: doc_id_md5(r.source_document),
     grade=lambda r: r.match_score,
-    on_duplicate="error"
+    on_duplicate="keep_max"
 )
 
 
